c4ad5ce927e1b1b79e9bfadefdc7ffa78f3b0f60
liang <hotzhao@gmail.com>
2018-02-07 11:14:19 +0800

    test a Japanese song

---
+++ play.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./sinsy-0.92/bin/sinsy \
+    -m ./hts_voice_nitech_jp_song070_f001-0.90/nitech_jp_song070_f001.htsvoice \
+    -o output.wav \
+    -x ./sinsy-0.92/dic/ \
+    ./song070_f00001_063.xml
